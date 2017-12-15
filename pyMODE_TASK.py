13f99f999b3c9b38a0a95f28946be8f8af0baeb4
Bilal Nizami <nizamibilal1064@gmail.com>
2017-12-15 20:35:40 +0530

    update pyMODE-TASK

--- pyMODE_TASK.py
+++ pyMODE_TASK.py
@@ -1509,7 +1509,6 @@ Research Unit in Bioinformatics (RUBi), Rhodes University, Grahamstown, South Af
 			visualiseVector_file_chk = path+'/src/visualiseVector.py'
 			getEigenVectors_file_chk = path+'/src/getEigenVectors'
 			ANM_file_chk = path+'/src/ANM'
-			#print ANM_file_chk
 			file_name_list = [pca_file_chk, intPca_file_chk, mds_file_chk, tsne_file_chk, pca_file_chk,\
 								ac_file_chk, cg_file_chk, com_mod_file_chk, conf_mod_file_chk, msf_file_chk,\
 								visualiseVector_file_chk, getEigenVectors_file_chk, ANM_file_chk]
